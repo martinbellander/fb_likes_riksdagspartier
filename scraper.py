7589c2b2dc4e9acd4ab8ee6ea7cf2f07fad9217d
martinbellander <martin.bellander@gmail.com>
2014-09-07 15:03:22 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import datetime
 
 now = datetime.datetime.now()
 
-partier = (["moderaterna", "folkpartiet", "Kristdemokraterna", "centerpartiet", "SDjatack", "miljopartiet", "socialdemokraterna", "vansterpartiet"]);
+partier = (["moderaterna", "folkpartiet", "Kristdemokraterna", "centerpartiet", "SDjatack", "miljopartiet", "socialdemokraterna", "vansterpartiet", "feministisktinitiativ"]);
 
 count = 1
 
